34f6021ec0ab4ae90b5af0609a61b1e7ece96ddf
rebecca <--global>
2017-03-13 18:52:32 -0700

    Code Files

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -1,1 +1,17 @@
 import pandas as pd
+
+def load_data_into_df(filepath):
+  df = pd.read_csv(filepath)
+  return df
+  
+def rename_columns(df):
+  pass
+
+def drop_nonpped_users(df):
+  
+  pass
+
+def create_recidivism_column(df):
+  y = df['total_recidivism_events'] > 0
+  return y
+
