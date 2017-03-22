c2facc62fc215daf59da1f72f08707efd6c54345
rebecca <--global>
2017-03-22 15:50:21 -0700

    Modified Flask App

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
-import _pickle as pickle 
+import _pickle as pickle
+ 
 app = Flask(__name__)
 @app.route("/")
 def main():
@@ -7,6 +8,7 @@ def main():
 @app.route("/about", methods=['GET', 'POST'])
 def about_page():
     return render_template('about.html')
+
 if __name__ == "__main__":
     app.run()
 
