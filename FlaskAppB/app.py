7ad5ea9df5fd4d1c57426cc70a85d1c98210d629
rebecca <--global>
2017-03-22 15:51:44 -0700

    New Flask App

---
+++ FlaskAppB/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+import _pickle as pickle
+ 
+app = Flask(__name__)
+@app.route("/")
+def main():
+    return render_template('index.html')
+@app.route("/about", methods=['GET', 'POST'])
+def about_page():
+    return render_template('about.html')
+
+if __name__ == "__main__":
+    app.run()
+
