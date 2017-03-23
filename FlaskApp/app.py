17c121ecf6ec5440b5b50b8a7f5ebd3f9de346ba
rebecca <--global>
2017-03-23 11:47:01 -0700

    Modified app.py

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import _pickle as pickle
  
 app = Flask(__name__)
@@ -8,7 +8,12 @@ def main():
 @app.route("/about", methods=['GET', 'POST'])
 def about_page():
     return render_template('about.html')
-
+@app.route('/calculate', methods=['POST'])
+def solve():
+    user_data = request.json
+    gender, education, race, age = user_data['gender'], user_data['race'], user_data['education'], user_data['age']
+    predicted_recidivism = model.predict_proba(gender, education, race, age)
+    return jsonify({'predicted_recidivism': predicted_recidivism})
 if __name__ == "__main__":
     app.run()
 
