f5d83f8634311a6d1c3c55f2e98d082e868d771b
rebecca <--global>
2017-03-23 13:07:01 -0700

    Added graphs onto webApp

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -1,6 +1,12 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, make_response
 import _pickle as pickle
- 
+from io import BytesIO
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+import random
+from IPython.html.widgets import interact
+
+
 app = Flask(__name__)
 @app.route("/")
 def main():
@@ -8,12 +14,31 @@ def main():
 @app.route("/about", methods=['GET', 'POST'])
 def about_page():
     return render_template('about.html')
-@app.route('/calculate', methods=['POST'])
-def solve():
-    user_data = request.json
-    gender, education, race, age = user_data['gender'], user_data['race'], user_data['education'], user_data['age']
-    predicted_recidivism = model.predict_proba(gender, education, race, age)
-    return jsonify({'predicted_recidivism': predicted_recidivism})
+
+@app.route('/plot.png')
+def plot():
+    fig = Figure()
+    axis = fig.add_subplot(1, 1, 1)
+    
+    cost = 51775
+    recidivism = 0.278
+    xs =[1,2,3]
+    ys = [i * cost * recidivism for i in xs]
+    
+    ppep_recidivism = 0.078
+    y2 = [i * cost* ppep_recidivism for i in xs]
+    
+
+    axis.plot(xs, ys)
+    axis.plot(xs, y2)
+    canvas = FigureCanvas(fig)
+    output = BytesIO()
+    canvas.print_png(output)
+    response = make_response(output.getvalue())
+    response.mimetype = 'image/png'
+    return response
+    
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
 
