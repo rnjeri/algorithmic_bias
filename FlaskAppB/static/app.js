c4f6a2ed32ef183bb66f01eba5a05bb0fd17b191
rebecca <--global>
2017-03-26 18:03:55 -0700

    app.js for working website

---
+++ FlaskAppB/static/app.js
@@ -0,0 +1,29 @@
+let get_responses = function() {
+  let gender = document.querySelector('input[name = "gender"]:checked').value
+  let race = document.querySelector('input[name = "race"]:checked').value
+ 	let age = $("input#age").val()
+    return {'gender': gender.toString(),
+      'race':race.toString(),
+		  'age': parseInt(age)}
+};
+let send_responses_json = function(responses) {
+    $.ajax({
+        url: '/calculate',
+        contentType: "application/json; charset=utf-8",
+        type: 'POST',
+        success: function (data) {
+            display_solutions(data);
+        },
+        data: JSON.stringify(responses)
+    });
+};
+let display_solutions = function(solutions) {
+    $("span#solution").html(solutions.predicted_recidivism)
+};
+
+$(document).ready(function() {
+    $("button#calculate").click(function() {
+        let responses = get_responses()
+        send_responses_json(responses);
+    })
+})
